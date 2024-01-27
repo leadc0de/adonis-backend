a4f625c960d1705380da529b59a2b866212f2852
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-01-27 11:38:40 +0100

    feat(user): add @adonisjs/bouncer

---
+++ apps/shared/policies/main.ts
@@ -0,0 +1,19 @@
+/*
+|--------------------------------------------------------------------------
+| Bouncer policies
+|--------------------------------------------------------------------------
+|
+| You may define a collection of policies inside this file and pre-register
+| them when creating a new bouncer instance.
+|
+| Pre-registered policies and abilities can be referenced as a string by their
+| name. Also they are must if want to perform authorization inside Edge
+| templates.
+|
+*/
+
+import {usersPolicies} from "#apps/users/policies/main";
+
+export const policies = {
+  ...usersPolicies
+}

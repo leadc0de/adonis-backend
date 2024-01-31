6c6b7ebec9732be9d89acd89ba7daa242ac40824
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-01-31 23:46:25 +0100

    feat(roles): added role routes

--- apps/users/routes.ts
+++ apps/users/routes.ts
@@ -2,10 +2,20 @@ import router from '@adonisjs/core/services/router'
 import {middleware} from "#start/kernel";
 
 const UsersController = () => import("#apps/users/controllers/users_controller")
+const RolesController = () => import("#apps/users/controllers/roles_controller")
 
 router.group(() => {
-  router.get('/', [UsersController, 'index'])
-  router.get('/:id', [UsersController, 'show'])
-  router.post('/', [UsersController, 'store'])
-}).prefix('users')
-  .use(middleware.auth())
+  router.group(() => {
+    router.get('/', [UsersController, 'index'])
+    router.get('/:id', [UsersController, 'show'])
+    router.post('/', [UsersController, 'store'])
+  }).prefix('users')
+
+  router.group(() => {
+    router.get('/', [RolesController, 'index'])
+    router.get('/:id', [RolesController, 'show'])
+    router.post('/', [RolesController, 'store'])
+    router.post('/:id', [RolesController, 'update'])
+    router.delete('/:id', [RolesController, 'destroy'])
+  }).prefix('roles')
+}).use(middleware.auth())

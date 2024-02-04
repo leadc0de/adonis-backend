81245363c701210cd1fab7773b0138a65624e0a0
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-02-04 17:19:59 +0100

    feat(blog): added get all route

--- apps/blog/routes.ts
+++ apps/blog/routes.ts
@@ -1,8 +1,14 @@
 import router from '@adonisjs/core/services/router'
 import CategoriesController from '#apps/blog/controllers/categories_controller'
+import ArticlesController from '#apps/blog/controllers/articles_controller'
 
 router.group(() => {
   router.group(() => {
     router.post('', [CategoriesController, 'store'])
   }).prefix('category')
+
+  router.group(() => {
+    router.get('', [ArticlesController, 'index'])
+    router.post('', [ArticlesController, 'store'])
+  }).prefix('articles')
 }).prefix('blog')
